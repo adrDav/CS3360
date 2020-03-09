06e3affcd9f078e7162cb3c9f5403f3aae8d9fbf
ken amamori <44108363+kmamamori@users.noreply.github.com>
2020-03-08 20:58:58 -0600

    Updated ./play/index.php

--- OmokService/src/play/index.php
+++ OmokService/src/play/index.php
@@ -1,78 +1,78 @@
 <?php
-if (!isset($_GET['pid'])) {
-	echo json_encode(
-		array(
-			'response' => false,
-			'reason' => 'Pid not specified'
-		)
-	);
-} else if (!isset($_GET['move'])) {
-	echo json_encode(
-		array(
-			'response' => false,
-			'reason' => 'Move not specified'
-		)
-	);
+require('validation.php');
+require('board.php');
+
+$isValid = validate();
+if (!$isValid['response']) {
+	echo json_encode($isValid);
 } else {
-	$pid = json_decode($_GET['pid']);
-	$move = json_decode($_GET['move']);
-	if (!file_exists('../data/' . $pid)) {
-		echo json_encode(
-			array(
-				'response' => false,
-				'reason' => 'Unknown pid'
-			)
-		);
-	} else if (strlen($move) != 3 || !is_numeric($move[0]) || !is_numeric($move[2]) || $move[1] == ',') {
-		echo json_encode(
-			array(
-				'response' => false,
-				'reason' => 'Move not well-formed'
-			)
-		);
-	} else if ($move[0] >= 0 && $move[0] < 16) {
-		echo json_encode(
-			array(
-				'response' => false,
-				'reason' => 'Invalid x coordinates'
-			)
-		);
-	} else if ($move[2] >= 0 && $move[2] < 15) {
-		echo json_encode(
-			array(
-				'response' => false,
-				'reason' => 'Invalid y coordinates'
-			)
-		);
-	} else {
-		$data = file_get_contents('../data/5e5c8d06e9255.json');
-		$jdata = json_decode($data, true);
-		$jdata['board'][$move[0]][$move[2]] = 1;
-		$x = -1;
-		$y = -1;
-		while ($x < 0 || $x > 14 || $y < 0 || $y > 14) {
-			$x = rand(0, 14);
-			$y = rand(0, 14);
-		}
-		$jdata['board'][$y][$y] = 2;
-		echo json_encode(
-			array(
-				'response' => true,
-				'ack_move' => array(
-					'x' => $move[0],
-					'y' => $move[2],
-					'isWin' => false,
-					'isDraw' => false,
-					'row' => array()
-				),
-				'move' => array(
-					'x' => $move[0],
-					'y' => $move[2],
-					'isWin' => false,
-					'isDraw' => false,
-					'row' => array()
-				)
-			)
-		);
+	// echo json_encode($_GET['pid']);
+	$board = new Board($_GET['pid']);
+	$pid = $_GET['pid'];
+	$move = $_GET['move'];
+	$data = file_get_contents('../data/'.$pid.'.json');
+
+	echo json_encode(
+		$board->getBoard()
+	);
+	echo '<h1>-</h1>';
+	$validate = validateIsNotPicked($move[0], $move[2], $board->getBoard());
+
+	if(!$validate['response']){
+		echo json_encode($isValid);
 	}
+	$board->updateUserInput($move[0], $move[2]);
+
+	// $jdata['board'][$move[0]][$move[2]] = 1;
+	// echo json_encode($jdata);
+	$x = -1;
+	$y = -1;
+	while ($x < 0 || $x > 14 || $y < 0 || $y > 14 /* TODO needs to be added validateCompInput($x, $y, $board->getBoard())*/) {
+		$x = rand(0, 14);
+		$y = rand(0, 14);
+	}
+	// $jdata['board'][$y][$y] = 2;
+	$board->updateCompInput($x, $y);
+	// echo '<h1>--------</h1>';
+	// echo json_encode($jdata);
+	
+	// $fp = fopen('../data/'.$pid.'.json', 'w');
+	// fwrite($fp, json_encode($jdata));
+	// fclose($fp);
+
+	echo json_encode(
+		$board->getBoard()
+	);
+
+	$board->storingData();
+
+	echo json_encode(
+		array(
+			'response' => true,
+			'ack_move' => array(
+				'x' => $move[0],
+				'y' => $move[2],
+				'isWin' => false, //make function
+				'isDraw' => false, //make function
+				'row' => array()
+			),
+			'move' => array(
+				'x' => $x,
+				'y' => $y,
+				'isWin' => false, //make function
+				'isDraw' => false, //make funtion
+				'row' => array()
+			)
+		)
+	);
+
+	/*
+	file_put_contents('../data/'.$pid.'.json', json_encode($jdata));
+*/
+
+	/*echo json_encode(
+		array(
+			'response' => true,
+		)
+	);*/
 }
