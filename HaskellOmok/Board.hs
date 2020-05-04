e67b0b868d3a1f964d889b07f3532921e25f9806
ken amamori <44108363+kmamamori@users.noreply.github.com>
2020-05-03 18:39:15 -0600

    Updated Board.hs; implemented some functions

--- HaskellOmok/Board.hs
+++ HaskellOmok/Board.hs
@@ -1,17 +1,40 @@
 module Board where
 
  -- mkBaord n
+ mkBoard n = mkBoardnxn n n
+ mkBoardnxn n nn = [[0|x<-[1..n]]|x<-[1..nn]]
+
  -- mkPlayer
+ mkPlayer = 1
+
  -- mkOpponent
+ mkOpponent = 2
+
  -- size bd
+ size bd = length bd
+
  -- row y bd
+ row y bd 
+  | y<1 || y>(size bd) = []
+  | otherwise = bd !! (y-1)
+
  -- column x bd
+ column x bd = [a !! (x-1) | a<-bd]
+
  -- mark x y bd p
+
+
  -- isEmpty x y bd
+ isEmpty x y bd = ((row y bd) !! (x-1) == 0)
+
  -- isMarked x y bd
+ isMarked x y bd = ((row y bd) !! (x-1) /= 0)
+
  -- isMarkedBy x y bd p
  -- marker x y board
  -- isFull bd
+  isFull bd = length (filter (\x->x==0)(concat bd)) == 0
+
  -- isWonBy bd p
  -- isDraw bd
  -- isGameOver bd
