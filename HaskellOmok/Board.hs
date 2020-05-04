89de22414c9d960158e9ddd4b32c9b5b96f18c6d
ken amamori <44108363+kmamamori@users.noreply.github.com>
2020-05-03 18:23:25 -0600

    Added signature of needed functions

--- HaskellOmok/Board.hs
+++ HaskellOmok/Board.hs
@@ -1,1 +1,19 @@
 module Board where
+
+ -- mkBaord n
+ -- mkPlayer
+ -- mkOpponent
+ -- size bd
+ -- row y bd
+ -- column x bd
+ -- mark x y bd p
+ -- isEmpty x y bd
+ -- isMarked x y bd
+ -- isMarkedBy x y bd p
+ -- marker x y board
+ -- isFull bd
+ -- isWonBy bd p
+ -- isDraw bd
+ -- isGameOver bd
+ -- boardToStr playerToChar bd
+
